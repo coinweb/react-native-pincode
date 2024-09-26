09a0a0ee2a38d46de7927dd2abaa4922d8ee0d50
dimisus <dimisus@coinweb.io>
2024-09-26 15:47:09 +0200

    fix: async-storage and react-move versions for fabric

--- index.tsx
+++ index.tsx
@@ -9,7 +9,7 @@ import {
     PinResultStatus,
 } from "./src/utils";
 
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as React from "react";
 import {
     View,
